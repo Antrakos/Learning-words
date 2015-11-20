061c17541f267f66b4819a7c15d5fc0c2df1b4bc
Antrakos <taraszubrei@gmail.com>
2015-11-21 00:01:48 +0200

    add new chalange

--- Word.py
+++ Word.py
@@ -17,18 +17,42 @@ class game:
     def __init__(self,path,count=10): #initializing game
         self.words = word(path)
         self.words.generateList(count)
-    def translate(self):
-        print("-"*5,"Start","-"*5)
+    def play(self,func):
         self.rightAnswersCount=0
-        for w,t in self.words.words.items(): #playing game
-            print("Word:\t\t\t" + t)
-            if w == str(input("Your answer:\t")):
+        for w,t in self.words.words.items():
+            f = getattr(self,func)
+            f(w,t)
+        print("Your result is: ",int(self.rightAnswersCount*100/self.words.count), "%")
+    def print(self,w,t):
+        print("Word:\t\t\t" + t)
+        if w == str(input("Your answer:\t")):
+            self.rightAnswersCount +=1 #if correct answer - increment player's result
+            print(bcolors.HEADER+"Correct!"+bcolors.ENDC)
+        else: print(bcolors.FAIL + "Incorrect!" + bcolors.ENDC + " Correct answer: ",bcolors.OKBLUE+w+bcolors.ENDC)
+
+    def choose(self,w,t):
+        print("Word:\t\t\t" + bcolors.BOLD +  w + bcolors.ENDC)
+        words = {t,}
+        while len(words) < 5:
+            words.update([choice(list(self.words.wordList.values()))])
+        words = list(words)
+        print(words)
+        try:
+            if t == words[int(input("Your answer: "))+1]:
                 self.rightAnswersCount +=1 #if correct answer - increment player's result
                 print(bcolors.HEADER+"Correct!"+bcolors.ENDC)
-            else: print(bcolors.FAIL + "Incorrect!" + bcolors.ENDC + " Correct answer: ",bcolors.OKBLUE+w+bcolors.ENDC)
-        print("Your result is: ",int(self.rightAnswersCount*100/self.words.count), "%") #show player's result
+            else: print(bcolors.FAIL + "Incorrect!" + bcolors.ENDC + " Correct answer: ",bcolors.OKBLUE+t+bcolors.ENDC)
+        except: print(bcolors.FAIL + "Incorrect!" + bcolors.ENDC + " Correct answer: ",bcolors.OKBLUE+t+bcolors.ENDC)
+
+class player:
+    def __init__(self,name):
+        self.name = name
+    def start(self):
+        print("-"*5,"Start","-"*5)
+        game.print()
+    def end(self):
         print("-"*5,"End","-"*5)
 
 if __name__ == '__main__':
     game = game('test.tsv')
-    game.translate()
+    game.play("choose")
